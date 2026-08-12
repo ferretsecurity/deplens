6dda6173c18fc1472812f788da1c5fd0befde5a1
Evgenii Protsenko <procenkoegg@gmail.com>
2026-08-13 01:19:04 +0200

    collect elixir-mix corpus examples (iteration 1)

---
+++ testdata/corpus/elixir-mix/candidate-34042bac68316fb38bd52c8e912ab572/mix.exs
@@ -0,0 +1,45 @@
+#-*-Mode:elixir;coding:utf-8;tab-width:2;c-basic-offset:2;indent-tabs-mode:()-*-
+# ex: set ft=elixir fenc=utf-8 sts=2 ts=2 sw=2 et nomod:
+
+defmodule Quickrand.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :quickrand,
+     version: "2.0.7",
+     language: :erlang,
+     erlc_options: [
+       {:d, :erlang.list_to_atom(~c"ERLANG_OTP_VERSION_" ++ :erlang.system_info(:otp_release))},
+       :deterministic,
+       :debug_info,
+       :warn_export_vars,
+       :warn_unused_import,
+       #:warn_missing_spec,
+       :warnings_as_errors],
+     description: description(),
+     package: package(),
+     deps: deps()]
+  end
+
+  def application do
+    [applications: [
+       :crypto]]
+  end
+
+  defp deps do
+    []
+  end
+
+  defp description do
+    "Quick Random Number Generation: " <>
+    "Provides a simple interface to call efficient random number generation " <>
+    "functions based on the context.  Proper random number seeding is enforced."
+  end
+
+  defp package do
+    [files: ~w(src doc rebar.config README.markdown LICENSE),
+     maintainers: ["Michael Truog"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/okeuday/quickrand"}]
+   end
+end
