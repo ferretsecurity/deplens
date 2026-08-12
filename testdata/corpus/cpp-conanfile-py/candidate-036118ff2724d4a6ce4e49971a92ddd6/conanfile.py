295cfaffefe1979bf9d25198fa5903a1f6f14651
Evgenii Protsenko <procenkoegg@gmail.com>
2026-08-13 00:04:48 +0200

    collect cpp-conanfile-py corpus examples (iteration 1)

---
+++ testdata/corpus/cpp-conanfile-py/candidate-036118ff2724d4a6ce4e49971a92ddd6/conanfile.py
@@ -0,0 +1,114 @@
+from conans import ConanFile, tools
+
+import os
+import platform
+import copy
+
+
+class DarwinToolchainConan(ConanFile):
+    name = "darwin-toolchain"
+    version = "1.0.8"
+    license = "Apple"
+    settings = "os", "arch", "build_type", "os_build", "compiler"
+    options = {"bitcode": [True, False]}
+    default_options = "bitcode=True"
+    description = "Darwin toolchain to (cross) compile macOS/iOS/watchOS/tvOS"
+    url = "https://www.github.com/theodelrieu/conan-darwin-tooolchain"
+    build_policy = "missing"
+    exports_sources = "darwin-toolchain.cmake"
+
+    @property
+    def cmake_system_name(self):
+        if self.settings.os == "Macos":
+            return "Darwin"
+        return str(self.settings.os)
+
+    @property
+    def cmake_system_processor(self):
+        return {"x86": "i386",
+                "x86_64": "x86_64",
+                "armv7": "arm",
+                "armv8": "aarch64"}.get(str(self.settings.arch))
+
+    def config_options(self):
+        # build_type is only useful for bitcode
+        if self.settings.os == "Macos":
+            del self.settings.build_type
+            del self.options.bitcode
+
+    def configure(self):
+        # We export recipes on a Linux machine, thus we have to rely on os_build and not sys.platform
+        if self.settings.os_build != "Macos":
+            raise Exception("Build machine must be Macos")
+        if not tools.is_apple_os(self.settings.os):
+            raise Exception("os must be an Apple os")
+        if self.settings.os in ["watchOS", "tvOS"] and not self.options.bitcode:
+            raise Exception("bitcode is required on watchOS/tvOS")
+        if self.settings.os == "watchOS" and self.settings.arch not in ["armv7k", "armv8", "x86", "x86_64"]:
+            raise Exception("watchOS: Only supported archs: [armv7k, armv8, x86, x86_64]")
+
+    def package(self):
+        self.copy("darwin-toolchain.cmake")
+
+    def package_info(self):
+        darwin_arch = tools.to_apple_arch(self.settings.arch)
+
+        if self.settings.os == "watchOS" and self.settings.arch == "armv8":
+            darwin_arch = "arm64_32"
+
+        xcrun = tools.XCRun(self.settings)
+        sysroot = xcrun.sdk_path
+
+        self.cpp_info.sysroot = sysroot
+
+        common_flags = ["-isysroot%s" % sysroot]
+
+        if self.settings.get_safe("os.version"):
+            common_flags.append(tools.apple_deployment_target_flag(self.settings.os, self.settings.os.version, os_sdk=self.settings.os.sdk))
+
+        if not self.settings.os == "Macos" and self.options.bitcode:
+            if self.settings.build_type == "Debug":
+                bitcode_flag = "-fembed-bitcode-marker"
+            else:
+                bitcode_flag = "-fembed-bitcode"
+            common_flags.append(bitcode_flag)
+
+        # CMake issue, for details look https://github.com/conan-io/conan/issues/2378
+        cflags = copy.copy(common_flags)
+        cflags.extend(["-arch", darwin_arch])
+        cxxflags = copy.copy(cflags)
+        self.cpp_info.cflags = cflags
+        self.cpp_info.cxxflags = cxxflags
+        link_flags = copy.copy(common_flags)
+        link_flags.append("-arch %s" % darwin_arch)
+
+        self.cpp_info.sharedlinkflags.extend(link_flags)
+        self.cpp_info.exelinkflags.extend(link_flags)
+
+        # Set flags in environment too, so that CMake Helper finds them
+        cflags_str = " ".join(cflags)
+        cxxflags_str = " ".join(cxxflags)
+        ldflags_str = " ".join(link_flags)
+        self.env_info.CC = xcrun.cc
+        self.env_info.CPP = "%s -E" % xcrun.cc
+        self.env_info.CXX = xcrun.cxx
+        self.env_info.AR = xcrun.ar
+        self.env_info.RANLIB = xcrun.ranlib
+        self.env_info.STRIP = xcrun.strip
+
+        self.env_info.CFLAGS = cflags_str
+        self.env_info.ASFLAGS = cflags_str
+        self.env_info.CPPFLAGS = cflags_str
+        self.env_info.CXXFLAGS = cxxflags_str
+        self.env_info.LDFLAGS = ldflags_str
+
+        self.env_info.CONAN_CMAKE_SYSTEM_NAME = self.cmake_system_name
+        if self.settings.get_safe("os.version"):
+            self.env_info.CONAN_CMAKE_OSX_DEPLOYMENT_TARGET = str(self.settings.os.version)
+        self.env_info.CONAN_CMAKE_OSX_ARCHITECTURES = str(darwin_arch)
+        self.env_info.CONAN_CMAKE_OSX_SYSROOT = sysroot
+        self.env_info.CONAN_CMAKE_SYSTEM_PROCESSOR = self.cmake_system_processor
+        self.env_info.CONAN_CMAKE_TOOLCHAIN_FILE = os.path.join(self.package_folder, "darwin-toolchain.cmake")
+
+    def package_id(self):
+        self.info.header_only()
---
+++ testdata/corpus/cpp-conanfile-py/candidate-3740c8e893d5a8e39ecb275bdbfc357b/conanfile.py
@@ -0,0 +1,147 @@
+import os
+import re
+import sys
+
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.apple import fix_apple_shared_install_name
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.files import copy, rm
+from conan.tools.scm import Version
+
+from version import get_version
+
+required_conan_version = ">=1.60.0 <2 || >=2.0.6"
+
+
+class LibqasmConan(ConanFile):
+    name = "libqasm"
+    version = get_version()
+    license = "Apache-2.0"
+    homepage = "https://github.com/QuTech-Delft/libqasm"
+    url = "https://github.com/conan-io/conan-center-index"
+    description = "Library to parse cQASM files"
+    topics = ("code generation", "parser", "compiler", "quantum compilation", "quantum simulation")
+    settings = "os", "compiler", "build_type", "arch"
+    package_type = "library"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "asan_enabled": [True, False],
+        "build_python": [True, False],
+        "cqasm_python_dir": [None, "ANY"],
+        "python_dir": [None, "ANY"],
+        "python_ext": [None, "ANY"]
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "asan_enabled": False,
+        "build_python": False,
+        "cqasm_python_dir": None,
+        "python_dir": None,
+        "python_ext": None
+    }
+    exports = "version.py", "include/libqasm/versioning.hpp"
+    exports_sources = "CMakeLists.txt", "include/*", "python/*", "res/*", "scripts/*", "src/*", "test/*"
+
+    @property
+    def _should_build_test(self):
+        return not self.conf.get("tools.build:skip_test", default=True, check_type=bool)
+
+    @property
+    def _min_cppstd(self):
+        return "20"
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "10",
+            "clang": "13",
+            "apple-clang": "14",
+            "Visual Studio": "16",
+            "msvc": "192"
+        }
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+        if self.settings.arch == "wasm":
+            self.options["antlr4-cppruntime"].shared = self.options.shared
+
+    def layout(self):
+        self.folders.source = "."
+        self.folders.build = os.path.join("build", str(self.settings.build_type))
+        self.folders.generators = os.path.join(self.folders.build, "generators")
+
+        self.cpp.package.libs = ["cqasm"]
+        self.cpp.package.includedirs = ["include"]
+        self.cpp.package.libdirs = ["lib"]
+
+        self.cpp.source.includedirs = ["include"]
+        self.cpp.build.libdirs = ["."]
+
+    def build_requirements(self):
+        self.tool_requires("tree-gen/1.0.9")
+        if self.settings.arch != "armv8":
+            self.tool_requires("zulu-openjdk/21.0.1")
+        if self.settings.arch == "wasm":
+            self.tool_requires("emsdk/3.1.50")
+        if self._should_build_test:
+            self.test_requires("gtest/1.15.0")
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(f"{self.ref} requires C++{self._min_cppstd},"
+                                            f"which your compiler does not support.")
+
+        if self.settings.arch != "wasm" and self.dependencies["antlr4-cppruntime"].options.shared != self.options.shared:
+            raise ConanInvalidConfiguration(f"{self.ref} requires antlr4-cppruntime to be built with the same shared option value.")
+
+    def requirements(self):
+        self.requires("fmt/11.0.2", transitive_headers=True)
+        self.requires("range-v3/0.12.0", transitive_headers=True)
+        self.requires("tree-gen/1.0.9", transitive_headers=True, transitive_libs=True)
+        if not self.settings.arch == "wasm":
+            self.requires("antlr4-cppruntime/4.13.1", transitive_headers=True)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.variables["ASAN_ENABLED"] = self.options.asan_enabled
+        tc.variables["BUILD_SHARED_LIBS"] = self.options.shared
+        tc.variables["LIBQASM_BUILD_EMSCRIPTEN"] = self.settings.arch == "wasm"
+        tc.variables["LIBQASM_BUILD_PYTHON"] = self.options.build_python
+        tc.variables["LIBQASM_BUILD_TESTS"] = self._should_build_test
+        tc.variables["LIBQASM_CQASM_PYTHON_DIR"] = self.options.cqasm_python_dir
+        tc.variables["LIBQASM_PYTHON_DIR"] = self.options.python_dir
+        tc.variables["LIBQASM_PYTHON_EXT"] = self.options.python_ext
+        tc.variables["PYTHON_EXECUTABLE"] = re.escape(sys.executable)
+        tc.generate()
+        env = VirtualBuildEnv(self)
+        env.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, "LICENSE.md", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+        fix_apple_shared_install_name(self)
+        rm(self, "*.pdb", self.package_folder, recursive=True)
+
+    def package_info(self):
+        self.cpp_info.libs = ["cqasm"]
