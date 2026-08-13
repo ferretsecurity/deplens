9a127e4a3505a0b8cc40c63ebba23fab9b5285cd
Evgenii Protsenko <procenkoegg@gmail.com>
2026-08-13 08:38:12 +0200

    collect zig-build corpus examples (iteration 1)

---
+++ testdata/corpus/zig-build/candidate-4bc7ae9e175e00ef79650ceb28018bf2/build.zig
@@ -0,0 +1,61 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    const source_files = &.{"src/ipcrypt2.c"};
+    lib_mod.addCSourceFiles(.{ .files = source_files });
+
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "ipcrypt2",
+        .root_module = lib_mod,
+        .version = .{ .major = 1, .minor = 1, .patch = 12 },
+    });
+
+    b.installArtifact(lib);
+
+    b.installDirectory(.{
+        .install_dir = .header,
+        .install_subdir = "",
+        .source_dir = b.path("src/include"),
+    });
+
+    const translate_c = b.addTranslateC(.{
+        .root_source_file = b.path("src/include/ipcrypt2.h"),
+        .target = target,
+        .optimize = optimize,
+    });
+    translate_c.addIncludePath(b.path("src/include"));
+
+    const main_tests = b.addTest(
+        .{ .root_module = b.createModule(
+            .{
+                .root_source_file = b.path("src/test/main.zig"),
+                .target = target,
+                .optimize = optimize,
+                .imports = &.{
+                    .{ .name = "ipcrypt2", .module = translate_c.createModule() },
+                },
+            },
+        ) },
+    );
+
+    main_tests.root_module.addIncludePath(b.path("src/include"));
+    main_tests.root_module.linkLibrary(lib);
+    if (target.result.os.tag == .windows) {
+        main_tests.root_module.linkSystemLibrary("ws2_32", .{});
+    }
+
+    const run_main_tests = b.addRunArtifact(main_tests);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&run_main_tests.step);
+}
